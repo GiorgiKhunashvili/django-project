e7a20d33865658f04716c6327972d18cb69e28ce
giorgi <giorgi.khunashvili.1@btu.edu.ge>
2019-11-26 14:13:33 +0400

    first commit

---
+++ users/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+import os
+# Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user = request.user
+        old_image_path = user.profile.image.path
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            if not old_image_path.endswith("media/default.jpg"):
+                os.remove(old_image_path)
+            messages.success(request, "Your account has been updated!")
+            return redirect('profile')
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form
+    }
+
+    return render(request, 'users/profile.html', context)
